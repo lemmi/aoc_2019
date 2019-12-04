1097a8607d816a165727cf2a5afc2380ca63fedc
lemmi <lemmi@nerd2nerd.org>
2019-12-04 02:15:27 +0100

    day 01

---
+++ 01/src/main.rs
@@ -0,0 +1,33 @@
+use std::io;
+use aoc2019::lines;
+
+fn fuel(mass: i32) -> i32 {
+    let f = (mass / 3) - 2;
+    if f < 0 {
+        0
+    } else {
+        f
+    }
+}
+
+fn fuelfuel(mass: i32) -> i32 {
+    if mass <= 0 {
+        0
+    } else {
+        let f = fuel(mass);
+        f + fuelfuel(f)
+    }
+}
+
+fn star1() -> io::Result<i32> {
+    Ok(lines("input")?.map(fuel).sum())
+}
+fn star2() -> io::Result<i32> {
+    Ok(lines("input")?.map(fuelfuel).sum())
+}
+
+fn main() -> io::Result<()> {
+    println!("Star 01: {}", star1()?);
+    println!("Star 02: {}", star2()?);
+    Ok(())
+}
