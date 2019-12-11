1232221e530ee232197a872ee8c4de2dd27b223a
lemmi <lemmi@nerd2nerd.org>
2019-12-11 08:06:27 +0100

    rewrite intcode to yield on input

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -1,9 +1,8 @@
 use aoc2019::lines;
+use aoc2019::intcode::State;
 use aoc2019::intcode::Intcode;
 use std::str::FromStr;
 use std::error::Error;
-use std::sync::{Arc,mpsc};
-use std::thread;
 
 use itertools::Itertools;
 
@@ -16,7 +15,7 @@ fn star1() -> Result<isize, Box<Error>> {
         let mut input = 0;
         for phase in phases {
             let mut amp = Intcode::from_str(&code)?;
-            input = *amp.run_input(&[phase, input])?.last().unwrap();
+            input = amp.run_input(&[phase, input]).out().pop().unwrap();
         }
 
         if input > max {
@@ -28,51 +27,31 @@ fn star1() -> Result<isize, Box<Error>> {
 }
 
 fn star2() -> Result<isize, Box<Error>> {
-    let code = Arc::new(lines("input")?.next().unwrap());
+    let code = lines("input")?.next().unwrap();
 
     let mut max = 0;
     for phases in (5..=9).permutations(5) {
-        let mut amps = Vec::new();
+        let mut amps = (0..5).into_iter().map(|_| Intcode::from_str(&code)).collect::<Result<Vec<_>,Box<Error>>>()?;
 
-        let (s0, ra) = mpsc::channel();
-        let (sa, rb) = mpsc::channel();
-        let (sb, rc) = mpsc::channel();
-        let (sc, rd) = mpsc::channel();
-        let (sd, re) = mpsc::channel();
-        let (se, r0) = mpsc::channel();
-        s0.send(phases[0])?;
-        s0.send(0)?;
-        sa.send(phases[1])?;
-        sb.send(phases[2])?;
-        sc.send(phases[3])?;
-        sd.send(phases[4])?;
-        let mut amp = Intcode::from_str(&code).unwrap();
-        amps.push(thread::spawn(move || { amp.run_channel(ra, sa); }));
-        let mut amp = Intcode::from_str(&code).unwrap();
-        amps.push(thread::spawn(move || { amp.run_channel(rb, sb); }));
-        let mut amp = Intcode::from_str(&code).unwrap();
-        amps.push(thread::spawn(move || { amp.run_channel(rc, sc); }));
-        let mut amp = Intcode::from_str(&code).unwrap();
-        amps.push(thread::spawn(move || { amp.run_channel(rd, sd); }));
-        let mut amp = Intcode::from_str(&code).unwrap();
-        amps.push(thread::spawn(move || { amp.run_channel(re, se); }));
+        for (phase, amp) in phases.into_iter().zip(&mut amps) {
+            assert_eq!(State::Wait(vec![]), amp.run_input(&[phase]));
+        }
 
-        for out in r0 {
-            match s0.send(out) {
-                Ok(()) => (),
-                Err(_) => {
-                    if max < out {
-                        max = out
-                    }
-                    break;
+        let mut input = vec![0];
+        let mut halted = 0;
+        while halted == 0{
+            for amp in &mut amps {
+                match amp.run_input(&input) {
+                    State::Wait(o) => {input = o;},
+                    State::Halt(o,_) => {
+                        input = o;
+                        halted += 1;
+                    },
                 }
             }
         }
 
-
-        for a in amps.into_iter() {
-            a.join().unwrap();
-        }
+        max = std::cmp::max(max, input.pop().unwrap())
     }
     Ok(max)
 }
--- aoc2019/src/lib.rs
+++ aoc2019/src/lib.rs
@@ -51,14 +51,33 @@ impl From<(isize, isize)> for Point<isize> {
 pub mod intcode {
     use std::error::Error;
     use std::str::FromStr;
-    use std::sync::mpsc;
 
     pub struct Intcode {
         mem: Vec<isize>,
         pc: isize,
         base: isize,
-        input: mpsc::Receiver<isize>,
-        output: mpsc::Sender<isize>,
+    }
+
+    enum InstrState {
+        Run,
+        Wait,
+        Output(isize),
+        Halt(isize),
+    }
+
+    #[derive(Debug,PartialEq)]
+    pub enum State<T> {
+        Wait(T),
+        Halt(T,isize),
+    }
+    
+    impl<T> State<T> {
+        pub fn out(&self) -> &T {
+            match self {
+                State::Wait(t) => t,
+                State::Halt(t,_) => t,
+            }
+        }
     }
 
     impl Intcode {
@@ -94,95 +113,86 @@ pub mod intcode {
             let addr = self.param_addr(off,mode);
             &mut self.mem[addr]
         }
-        pub fn step(&mut self) -> Option<isize> {
-            //println!("{:?}", &self.mem[self.pc as usize .. self.pc as usize + 4]);
+        fn step(&mut self, input: &mut Iterator<Item = isize>) -> InstrState {
             match self.op() {
                 (1, p1, p2, p3) => {
                     *self.param_mut(3,p3) = self.param(1, p1) + self.param(2, p2);
                     self.pc += 4;
-                    None
+                    InstrState::Run
                 },
                 (2, p1, p2, p3) => {
                     *self.param_mut(3, p3) = self.param(1, p1) * self.param(2, p2);
                     self.pc += 4;
-                    None
+                    InstrState::Run
                 },
                 (3, p1, _, _) => {
-                    *self.param_mut(1,p1) = self.input.recv().unwrap();
-                    self.pc += 2;
-                    None
+                    if let Some(input) = input.next() {
+                        *self.param_mut(1,p1) = input;
+                        self.pc += 2;
+                        InstrState::Run
+                    } else {
+                        InstrState::Wait
+                    }
                 },
                 (4, p1, _, _) => {
                     let data = self.param(1,p1);
-                    self.output.send(data).unwrap();
                     self.pc += 2;
-                    None
+                    InstrState::Output(data)
                 },
                 (5, p1, p2, _) => {
                     match self.param(1,p1) {
                         0 => { self.pc += 3 },
                         _ => { self.pc = self.param(2,p2) },
                     }
-                    None
+                    InstrState::Run
                 },
                 (6, p1, p2, _) => {
                     match self.param(1,p1) {
                         0 => { self.pc = self.param(2,p2) },
                         _ => { self.pc += 3 },
                     }
-                    None
+                    InstrState::Run
                 },
                 (7, p1, p2, p3) => {
                     *self.param_mut(3,p3) = (self.param(1,p1) < self.param(2,p2)) as isize;
                     self.pc += 4;
-                    None
+                    InstrState::Run
                 },
                 (8, p1, p2, p3) => {
                     *self.param_mut(3,p3) = (self.param(1,p1) == self.param(2,p2)) as isize;
                     self.pc += 4;
-                    None
+                    InstrState::Run
                 },
                 (9, p1, _, _) => {
                     self.base += self.param(1,p1) as isize;
                     self.pc += 2;
-                    None
+                    InstrState::Run
                 },
-                (99, _, _, _) => Some(self.mem[0]),
+                (99, _, _, _) => InstrState::Halt(self.mem[0]),
                 (x, _, _, _) => panic!("Invalid opcode {}, pc: {}", x, self.pc),
             }
         }
         pub fn run(&mut self) -> Result<isize, Box<Error>> {
             loop {
-                match self.step() {
-                    None => (),
-                    Some(x) => return Ok(x),
+                match self.step(&mut std::iter::empty()) {
+                    InstrState::Run => (),
+                    InstrState::Wait => panic!("input needed"),
+                    InstrState::Output(_) => (),
+                    InstrState::Halt(x) => return Ok(x),
                 }
             }
         }
-        pub fn run_channel(&mut self, input: mpsc::Receiver<isize>, output: mpsc::Sender<isize>) {
-            self.input = input;
-            self.output = output;
-            self.base = 0;
+        pub fn run_input(&mut self, input: &[isize]) -> State<Vec<isize>> {
+            let mut out = Vec::new();
+            let mut in_iter = input.into_iter().cloned();
             loop {
-                match self.step() {
-                    None => (),
-                    Some(_) => return,
-                }
-            }
-        }
-        pub fn run_input(&mut self, input: &[isize]) -> Result<Vec<isize>, Box<Error>> {
-            let (si, ri) = mpsc::channel();
-            let (so, ro) = mpsc::channel();
-            self.input = ri;
-            self.output = so;
-            self.base = 0;
-            for i in input {
-                si.send(*i)?;
-            }
-            loop {
-                match self.step() {
-                    None => (),
-                    Some(_) => return Ok(ro.try_iter().collect())
+                match self.step(&mut in_iter) {
+                    InstrState::Run => (),
+                    InstrState::Wait => {
+                        return State::Wait(std::mem::replace(&mut out, Vec::new()));
+                    }
+                    InstrState::Output(o) => out.push(o),
+                    InstrState::Halt(h) => return State::Halt(std::mem::replace(&mut out, Vec::new()),h)
                 }
             }
         }
@@ -196,13 +206,10 @@ pub mod intcode {
         type Err = Box<Error>;
         fn from_str(src: &str) -> Result<Self, Self::Err> {
             let mem: Result<Vec<isize>, _> = src.split(",").map(|n| n.parse()).collect();
-            let (s,r) = mpsc::channel();
             Ok(Intcode {
                 mem: mem?,
                 pc: 0,
                 base: 0,
-                input: r,
-                output: s,
             })
         }
     }
