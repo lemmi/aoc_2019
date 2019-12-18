8199fef636d5f1654b1aa6e14f4b20158136719d
lemmi <lemmi@nerd2nerd.org>
2019-12-18 21:39:35 +0100

    cargo-fmt

--- aoc2019/src/lib.rs
+++ aoc2019/src/lib.rs
@@ -3,8 +3,7 @@ use std::io;
 use std::io::BufRead;
 use std::ops::Add;
 
-pub fn lines(filename: &str) -> io::Result<Box<dyn Iterator<Item = String>>>
-{
+pub fn lines(filename: &str) -> io::Result<Box<dyn Iterator<Item = String>>> {
     let f = File::open(filename)?;
     Ok(Box::new(
         io::BufReader::new(f)
@@ -57,8 +56,8 @@ pub fn gcd(a: isize, b: isize) -> isize {
     gcd(b, a % b)
 }
 
-pub fn lcm(a:isize,b:isize) -> isize {
-    (a*b).abs() / gcd(a,b)
+pub fn lcm(a: isize, b: isize) -> isize {
+    (a * b).abs() / gcd(a, b)
 }
 
 pub mod intcode {
@@ -78,26 +77,25 @@ pub mod intcode {
         Halt(isize),
     }
 
-    #[derive(Debug,PartialEq)]
+    #[derive(Debug, PartialEq)]
     pub enum State<T> {
         Wait(T),
-        Halt(T,isize),
+        Halt(T, isize),
     }
-    
+
     impl<T> State<T> {
         pub fn out(&self) -> &T {
             match self {
                 State::Wait(t) => t,
-                State::Halt(t,_) => t,
+                State::Halt(t, _) => t,
             }
         }
         pub fn is_halt(&self) -> bool {
             match self {
                 State::Wait(_) => false,
-                State::Halt(_,_) => true,
+                State::Halt(_, _) => true,
             }
         }
-
     }
 
     impl Intcode {
@@ -112,7 +110,7 @@ pub mod intcode {
         pub fn param_addr(&mut self, off: isize, mode: isize) -> usize {
             let immaddr = (self.pc + off) as usize;
             if immaddr >= self.mem.len() {
-                self.mem.resize(immaddr+1,0);
+                self.mem.resize(immaddr + 1, 0);
             }
             let addr = match mode {
                 0 => self.mem[immaddr] as usize,
@@ -121,73 +119,73 @@ pub mod intcode {
                 x => panic!("unknown parameter mode {}", x),
             };
             if addr >= self.mem.len() {
-                self.mem.resize(addr+1,0);
+                self.mem.resize(addr + 1, 0);
             }
             addr
         }
         pub fn param(&mut self, off: isize, mode: isize) -> isize {
-            let addr = self.param_addr(off,mode);
+            let addr = self.param_addr(off, mode);
             self.mem[addr]
         }
         pub fn param_mut(&mut self, off: isize, mode: isize) -> &mut isize {
-            let addr = self.param_addr(off,mode);
+            let addr = self.param_addr(off, mode);
             &mut self.mem[addr]
         }
         fn step(&mut self, input: &mut dyn Iterator<Item = isize>) -> InstrState {
             match self.op() {
                 (1, p1, p2, p3) => {
-                    *self.param_mut(3,p3) = self.param(1, p1) + self.param(2, p2);
+                    *self.param_mut(3, p3) = self.param(1, p1) + self.param(2, p2);
                     self.pc += 4;
                     InstrState::Run
-                },
+                }
                 (2, p1, p2, p3) => {
                     *self.param_mut(3, p3) = self.param(1, p1) * self.param(2, p2);
                     self.pc += 4;
                     InstrState::Run
-                },
+                }
                 (3, p1, _, _) => {
                     if let Some(input) = input.next() {
-                        *self.param_mut(1,p1) = input;
+                        *self.param_mut(1, p1) = input;
                         self.pc += 2;
                         InstrState::Run
                     } else {
                         InstrState::Wait
                     }
-                },
+                }
                 (4, p1, _, _) => {
-                    let data = self.param(1,p1);
+                    let data = self.param(1, p1);
                     self.pc += 2;
                     InstrState::Output(data)
-                },
+                }
                 (5, p1, p2, _) => {
-                    match self.param(1,p1) {
-                        0 => { self.pc += 3 },
-                        _ => { self.pc = self.param(2,p2) },
+                    match self.param(1, p1) {
+                        0 => self.pc += 3,
+                        _ => self.pc = self.param(2, p2),
                     }
                     InstrState::Run
-                },
+                }
                 (6, p1, p2, _) => {
-                    match self.param(1,p1) {
-                        0 => { self.pc = self.param(2,p2) },
-                        _ => { self.pc += 3 },
+                    match self.param(1, p1) {
+                        0 => self.pc = self.param(2, p2),
+                        _ => self.pc += 3,
                     }
                     InstrState::Run
-                },
+                }
                 (7, p1, p2, p3) => {
-                    *self.param_mut(3,p3) = (self.param(1,p1) < self.param(2,p2)) as isize;
+                    *self.param_mut(3, p3) = (self.param(1, p1) < self.param(2, p2)) as isize;
                     self.pc += 4;
                     InstrState::Run
-                },
+                }
                 (8, p1, p2, p3) => {
-                    *self.param_mut(3,p3) = (self.param(1,p1) == self.param(2,p2)) as isize;
+                    *self.param_mut(3, p3) = (self.param(1, p1) == self.param(2, p2)) as isize;
                     self.pc += 4;
                     InstrState::Run
-                },
+                }
                 (9, p1, _, _) => {
-                    self.base += self.param(1,p1) as isize;
+                    self.base += self.param(1, p1) as isize;
                     self.pc += 2;
                     InstrState::Run
-                },
+                }
                 (99, _, _, _) => InstrState::Halt(self.mem[0]),
                 (x, _, _, _) => panic!("Invalid opcode {}, pc: {}", x, self.pc),
             }
@@ -212,7 +210,9 @@ pub mod intcode {
                         return State::Wait(std::mem::replace(&mut out, Vec::new()));
                     }
                     InstrState::Output(o) => out.push(o),
-                    InstrState::Halt(h) => return State::Halt(std::mem::replace(&mut out, Vec::new()),h)
+                    InstrState::Halt(h) => {
+                        return State::Halt(std::mem::replace(&mut out, Vec::new()), h)
+                    }
                 }
             }
         }
@@ -224,7 +224,10 @@ pub mod intcode {
             &mut self.mem[idx]
         }
         pub fn from_file(f: &str) -> Result<Self, Box<dyn Error>> {
-            super::lines(f)?.next().ok_or("no code available".to_string())?.parse()
+            super::lines(f)?
+                .next()
+                .ok_or("no code available".to_string())?
+                .parse()
         }
     }
     impl FromStr for Intcode {
